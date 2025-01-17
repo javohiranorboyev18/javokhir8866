828aa25e7e94fe9b26ef4dda914c61bb58f1c688
javohir88866 <j.anorboyev@newuu.uz>
2025-01-17 14:46:06 +0500

    My fist commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    cout  << "What is your name?" << endl;
+    string name;
+    cin >> name;
+    cout << "My name is " <<name << endl;
+    return 0;
+}
