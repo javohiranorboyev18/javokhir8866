e2d81254de85bbe578e978ee7de8de3d5fbf2b7e
Second Lab <j.anorboyev@newuu.uz>
2025-01-23 12:31:15 +0500

    Second Lab

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main() {
+    //PROBLEM 1
+    // string name;
+    // int age;
+    // cout << "Please enter your name: ";
+    // cin >> name;
+    // cout << "How old are you? : ";
+    // cin >> age;
+    // cout << "Ok, " << name << ". You are " << age << " years old." << endl;
+    // PROBLEM 2
+    // for (int i = 1; i <= 5; ++i) {
+    //     cout << setw(5) << i << endl;
+    // }
+    // PROBLEM 3
+    //     int a, b;
+    //     cout << "Enter two integers: ";
+    //     cin >> a >> b;
+    //     int temp = a;
+    //     a = b;
+    //     b = temp;
+    //     cout << "Swapped values: " << "a = " << a << ", b = " << b << endl;
+    // PROBLEM 4
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    //
+    // for (int i = 1; i <= n; i++) {
+    //     cout <<"Number: "<< i << setw(20) <<"Square: " << i * i << endl;
+    //
+    // }
+    //PROBLEM 5
+//     string something;
+//     cout << "Write something: ";
+//     cin >> something;
+//     int a=something.length();
+//     cout << setw ((50-a)/2) << something << setw((50-a)/2) << endl;
+// }
+
+
+
+
