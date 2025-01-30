e82ea4cab14335a88e93a01922de14399b461f8e
Second Lab <j.anorboyev@newuu.uz>
2025-01-30 12:34:48 +0500

    Second Lab

--- main.cpp
+++ main.cpp
@@ -4,42 +4,152 @@
 using namespace std;
 int main() {
     //PROBLEM 1
-    // string name;
-    // int age;
-    // cout << "Please enter your name: ";
-    // cin >> name;
-    // cout << "How old are you? : ";
-    // cin >> age;
-    // cout << "Ok, " << name << ". You are " << age << " years old." << endl;
-    // PROBLEM 2
-    // for (int i = 1; i <= 5; ++i) {
-    //     cout << setw(5) << i << endl;
-    // }
-    // PROBLEM 3
-    //     int a, b;
-    //     cout << "Enter two integers: ";
-    //     cin >> a >> b;
-    //     int temp = a;
-    //     a = b;
-    //     b = temp;
-    //     cout << "Swapped values: " << "a = " << a << ", b = " << b << endl;
-    // PROBLEM 4
-    // int n;
-    // cout << "Enter a number: ";
-    // cin >> n;
+    // int q,w,e,r,t, sum;
+    // cout << "Enter first number ";
+    // cin>>q;
+    // cout << "Enter second number ";
+    // cin>>w;
+    // cout << "Enter third number ";
+    // cin>>e;
+    // cout << "Enter fourth number ";
+    // cin>>r;
+    // cout << "Enter fifth number ";
+    // cin >> t;
+    // sum = w + e + r + t;
+    // cout << "Sum of all numbers is " << sum << endl;
+    //PROBLEM 2
+    // int id, year, fee;
+    // double gpa;
+    // cout << "Enter your id: ";
+    // cin >> id;
+    // cout << "Enter your gpa: ";
+    // cin >> gpa;
+    // cout << "Enter your year: ";
+    // cin >> year;
+    // cout << "Enter your fee: ";
+    // cin >> fee;
+    // cout << "Student ID: " << id << endl;
+    // cout << "Student GPA: " << gpa << endl;
+    // cout << "Student Year of Study: " << year << endl;
+    // cout <<"Student Study fee: " << fee << endl;
+    //PROBLEM 3
+    // int c;
+    // float f;
+    // cout << "Enter the temperature in Celsius: ";
+    // cin >> c;
+    // f = (c*9/5)+32;
+    // cout << "The temperature in Celsius : " << c << endl;
+    // cout << "The temperature in Fahrenheit : " << f << endl;
+    // PROBLEM 5
+    // int a1, a2,a3,a4;
+    // double all;
+    // cout << "Enter the first member's age: ";
+    // cin >> a1;
+    // cout << "Enter the second member's age: ";
+    // cin >> a2;
+    // cout << "Enter the third member's age: ";
+    // cin >> a3;
+    // cout << "Enter the fourth member's age: ";
+    // cin >> a4;
+    // all = (a1 + a2 + a3 + a4)/4.0;
+    // cout << "Avarage age is " << all << endl;
+    //PROBLEM 6
+//     int lenght,width, area,perimetr;
+//     cout << "Enter the length of the rectangle: ";
+// cin >> lenght;
+//     cout << "Enter the width of the rectangle: ";
+//     cin >> width;
+//     area = lenght * width;
+//     perimetr = (area + lenght)*2;
+// cout << "Area of the rectangle: " << area << endl;
+//     cout << "Perimetr of the rectangle: " << perimetr << endl;
+
+//PROBLEM7
+    // int dividen,divisor, quotient, remainder;
     //
-    // for (int i = 1; i <= n; i++) {
-    //     cout <<"Number: "<< i << setw(20) <<"Square: " << i * i << endl;
+    // cout<<"Enter dividen: ";
+    // cin>>dividen;
+    // cout<<"Enter divisor: ";
+    // cin>>divisor;
+    // quotient=dividen/divisor;
+    // remainder=dividen%divisor;
+    // cout<<"Quotient: "<<quotient<<endl;
+    // cout<<"Remainder: "<<remainder<<endl;
     //
-    // }
-    //PROBLEM 5
-//     string something;
-//     cout << "Write something: ";
-//     cin >> something;
-//     int a=something.length();
-//     cout << setw ((50-a)/2) << something << setw((50-a)/2) << endl;
-// }
 
+    //PROBLEM8
+    // int usd,uzs;
+    // cout << "Enter USD:";
+    // cin >> usd;
+    // uzs= usd*12500;
+    // cout << "Amount in USD: " << usd << endl;
+    // cout << "Amount in UZS: " << uzs << endl;
 
+    //PROBLEM9
+    // int r;
+    // float area;
+    // cout << "Enter the radius of the circle: ";
+    // cin >> r;
+    // area = 3.14 * r * r;
+    // cout << "The area of the circle is " << area << endl;
+    //PROBLEM10
+    // int h,r;
+    // float volume;
+    // cout << "Enter height:";
+    // cin >> h;
+    // cout << "Enter radius:";
+    // cin >> r;
+    // volume = 3.14*h*r*r;
+    // cout << "Volume of the cylinder is " << volume << endl;
+    //PROBLEM11
+// int distance,hours;
+//     float avarage, in_mile;
+//     distance=450;
+//    hours=2;
+//     avarage= distance/2;
+//     in_mile= avarage*0.621;
+//     cout << "Avarage speed: " << avarage << endl;
+//     cout << "Avarage inmiles: " << in_mile << endl;
+//
+//PROBLEM12
+    // int minutes,remaining;
+    // cout << "Enter minutes: ";
+    // cin >> minutes;
+    // float hours=minutes/60;
+    // remaining=minutes%60;
+    // cout << "Total minutes: " << minutes << endl;
+    // cout << "Total time: " << hours << " hours " << remaining << " minutes" << endl;
+//PROBLEM13
+  //   int fixed, variable, price,bep;
+  //   cout << "Enter selling price per unit: ";
+  //   cin >> price;
+  //   cout << "Enter variable cost: ";
+  //   cin >> variable;
+  //
+  //   fixed = 10000;
+  // bep = fixed/(price - variable);
+  //   cout << "Break-even point: " << bep << endl;
 
+    //PROBLEM14
+    // int salary,tax_rate,net_salary;
+    // cout << "Enter your salary: ";
+    // cin >> salary;
+    // tax_rate = 12/100;
+    // net_salary = salary*(1-tax_rate);
+    // cout << "Total salary: " << net_salary << endl;
 
+    // int original_price,discount_percentage;
+    // cout << "Enter original price: ";
+    // cin >> original_price;
+    // cout << "Enter discount percentage: ";
+    // cin >> discount_percentage;
+    //
+    // int discount_price = original_price * (1-discount_percentage/100);
+    // int discount_amount = original_price * discount_percentage / 100;
+    // int final_price = original_price - original_price*discount_percentage/100;
+    // cout << "Original Price: " << original_price << endl;
+    // cout << "Discount percentage: " << discount_percentage << endl;
+    // cout << "Discount amount: " << discount_amount << endl;
+    // cout << "Final Price: " << final_price <<  endl;
+return 0;
+}
