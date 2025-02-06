e7e27673e2029c1fd35bf4227585d89634b0c8fe
Second Lab <j.anorboyev@newuu.uz>
2025-02-06 11:35:38 +0500

    LAB3

---
+++ main.cpp
@@ -0,0 +1,202 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    // PROBLEM1
+
+    // cout << "Enter any number:";
+    // int n;
+    // cin >> n;
+    // if (n > 0 )
+    // cout << "Output: Positive number" << endl;
+    // else if (n < 0)
+    // cout << "Output: Negative number" << endl;
+    // cout << "Input: " << n << endl;
+
+    // PROBLEM2
+
+    //     cout << "Enter any number:";
+    // int n;
+    // cin >> n;
+    // if (n%2 == 0)
+    // cout << "Even" << endl;
+    // else if (n%1 ==1)
+    // cout << "Odd" << endl;
+    // cout <<"Input: " << n << endl;
+
+    //PROBLEM3
+
+    // cout << "Enter first number: ";
+    // int n;
+    // cin >> n;
+    // cout << "Enter second number: ";
+    // int n1;
+    // cin >> n1;
+    // if (n>n1)
+    // cout << "Largest number: " << n << endl;
+    // else (n<n1);
+    // cout << "Largest number: " << n1 << endl;
+
+    //PROBLEM4
+
+    // int n,n1,n2;
+    // cout << "Enter first number: ";
+    // cin >> n;
+    // cout << "Enter second number: ";
+    // cin >> n1;
+    // cout << "Enter third number: ";
+    // cin >> n2;
+    // if (n>n1 && n>n2)
+    // cout << "Largest number: " << n << endl;
+    // else if (n1>n && n1>n2)
+    // cout << "Largest number: " << n1 << endl;
+    // else if (n2>n && n2>n)
+    // cout << "Largest number: " << n2 << endl;
+    // cout << "Input: " << n << ", " << n1 << ", " << n2 << endl;
+    //PROBLEM5
+    // cout << "Enter year: " << endl;
+    // int year;
+    // cin >> year;
+    // if (year % 4 == 0)
+    // cout << "It is leap year";
+    // else cout  << "It is not leap year ";
+    // cout << year << endl;
+    //PROBLEM6
+    // int speed;
+    // cout << "Enter speed: ";
+    // cin >> speed;
+    // if (speed < 20)
+    // cout << "Too slow";
+    // else if (speed>80)
+    // cout << "Too fast";
+    // else if (
+    // cout << "just right";
+    // cout << speed<< endl)
+    //PROBLEM7
+    //     int mark;
+    // cout << "Enter your mark: ";
+    // cin >> mark;
+    // if (mark  >= 50)
+    // cout << "Pass " <<endl;
+    // else if (mark <= 49)
+    // cout << "Fail " << endl;
+    // cout << mark << endl;
+    //PROBLEM8
+    // int num;
+    // cout << "Enter a number: ";
+    // cin >> num;
+    // if (num==0)
+    // cout << "zero"<< endl;
+    // else if (num > 0)
+    // cout << "positive" << endl;
+    // else if (num < 0)
+    // cout << "negative" << endl;
+    // cout << num << endl;
+    //PROBLEM9
+    //PROBLEM10
+    // cout << "Enter your grade: " << endl;
+    // int grade;
+    // cin >> grade;
+    // if (grade >= 90 && grade <= 100)
+    // cout << "Your scale id 'A'" << endl;
+    // else if (grade <=89 && grade >= 80)
+    // cout << "Your scale id 'B'" << endl;
+    // else if (grade <=79 && grade >= 70)
+    // cout << "Your scale id 'C'" << endl;
+    // else if (grade <=69 && grade >= 60)
+    // cout << "Your scale id 'D'" << endl;
+    // else if (grade <=59 && grade >= 0)
+    // cout << "You Failed Bro" << endl;
+    // cout << grade << endl;
+    //PROBLEM11
+    // cout << "Enter first numer: " << endl;
+    // int n;
+    // cin >> n;
+    // cout << "Enter second numer: " << endl;
+    // int n2;
+    // cin >> n2;
+    // if (n % n2 == 0)
+    // cout << n << " is divisible by " << n2 << endl;
+    // else if (n % n2 != 0)
+    // cout << n << " is not divisible by " << n2 << endl;
+    // cout <<endl;
+    //PROBLEM12
+    //cout << "Enter first side of triangle: " << endl;
+    //int side;
+    //cin >> side;
+    //   cout << "Enter second side of triangle: " << endl;
+    //int side2;
+    //cin >> side2;
+    //cout << "Enter third side of triangle: " << endl;
+    //int side3;
+    //cin >> side3;
+    //if (side +side2 +side3 == 180)
+    //cout << "The triangle is valid" << endl;
+    //else
+    //cout << "The triangle is not valid" << endl;
+    //PROBLEM13
+    // char a;
+    // cout << "Enter a character: ";
+    // cin >> a;
+    // if (a >= 'a' && a <= 'z')
+    //     cout << "Lowercase alphabet" << endl;
+    // else if (a >= 'A' && a <= 'Z')
+    //     cout << "Uppercase alphabet" << endl;
+    // else
+    // cout  << "It is not an alphabet" << endl;
+    // cout << endl;
+
+
+
+
+    //PROBLEM14
+    //cout << "Enter weight and price for package 1: "<< endl;
+    //int weight, price;
+    //cin >> weight >> price;
+    //cout << "Enter weight and price for package 2: "<< endl;
+    //int weight2, price2;
+    //cin >> weight >> price2;
+    //if (weight/price > weight2/price2)
+    //cout << "Package 1 has a better price";
+    //else if (weight/price < weight2/price2)
+    //cout << "Package 2 has a better price";
+    //cout << endl;
+
+    //PROBLEM15
+    // int num,first_digit,last_digit;
+    // cout<<"Enter the number: ";
+    // cin>>num;
+    // first_digit=num/100;
+    // last_digit=num%10;
+    // if (first_digit==last_digit)
+    //     cout<<num << " is a palindrome"<< endl;
+    // else
+    //     cout<<num << " is not a palindrome"<< endl;
+    //PROBLEM16
+//     cout << "Enter point (x,y): ";
+//     int x,y;
+//     cin >> x >> y;
+//     if (x>5 || y>5)
+//         cout << "Point ("<< x << "," << y<< ") is not in circle" << endl;
+// else if (x<=5 || y<=5)
+//     cout << "Point ("<< x << "," << y<< "is in circle" << endl;
+//     else if (x<= -5 || y<= -5)
+//     cout << "Point ("<< x << "," << y<< "is not in circle" << endl;
+//     else if (x> -5 || y> -5)
+//         cout << "Point ("<< x << "," << y<< "is in circle" << endl;
+    //PROBLEM17
+//     cout << "Enter your GPA: ";
+//     double GPA;
+//     cin >> GPA;
+//     if (GPA > 4.0 && GPA < 4.5)
+//         cout << "You got 80% scholarship" << endl;
+//     else if (GPA > 3.5 && GPA < 4.0)
+//         cout << "You got 60% scholarship" << endl;
+//     else if (GPA > 3.0 && GPA < 3.5)
+//         cout << "You got 50% scholarship" << endl;
+//     else if (GPA < 3.0)
+//         cout << "You did not get scholarship" << endl;
+// cout << endl;
+
+    return 0;
+}
